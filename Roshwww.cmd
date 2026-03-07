b2771869ba01d0f49eca1b93acea67b128028a0d
drerx <119292904+drerx@users.noreply.github.com>
2026-03-07 09:40:15 +0300

    Add files via upload

---
+++ Roshwww.cmd
@@ -0,0 +1,4 @@
+start "" "C:\Progra~1\Google\Chrome\Applic~1\chrome.exe"  --disable-web-security  --new-window http://127.0.0.1:8080/index.html & start "" python -m http.server 8080 --bind 127.0.0.1 --directory G:\RoshwwwV17
+
+
+
